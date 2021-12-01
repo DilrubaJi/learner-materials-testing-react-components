3d4cf222d51731cd8f75b1d775174d0f538ac25f
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-12-01 02:31:49 +0000

    all tests passed - speciesName component to render

--- src/components/SpeciesName.test.js
+++ src/components/SpeciesName.test.js
@@ -4,7 +4,7 @@ import SpeciesName from './SpeciesName';
 test('renders label text', () => {
     render(<SpeciesName />);
     const labelText = screen.getByText(
-        /Random text to fail test/i
+        /Species Name:/i
     );
     expect(labelText).toBeInTheDocument();
 });
